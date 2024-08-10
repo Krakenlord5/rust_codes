7636a698b6dd7358c7a9fb04a49193b450529b56
Krakenlord <chananyu1301@gmail.com>
2024-08-10 16:56:06 +0700

    unfinished

--- my_game/src/main.rs
+++ my_game/src/main.rs
@@ -1,27 +1,51 @@
-use std::io::{stdout, Write};
+use std::io::{self, Write};
+use std::{thread, time::Duration};
 
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use crossterm::execute;
 use crossterm::{
-    ExecutableCommand, execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    event,
+    style::{Stylize, Print},
+    cursor::{MoveTo, MoveLeft, MoveRight},
+    terminal::{Clear, ClearType, SetSize},
+    event::{read, poll, Event},
 };
 
-fn main() -> std::io::Result<()> {
-    // using the macro
-    execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
-        SetBackgroundColor(Color::Red),
-        Print("Styled text here."),
-        ResetColor
-    )?;
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+    let esc = KeyEvent::new(KeyCode::Char('a'), KeyModifiers::NONE);
 
-    // or using functions
-    stdout()
-        .execute(SetForegroundColor(Color::Blue))?
-        .execute(SetBackgroundColor(Color::Red))?
-        .execute(Print("Styled text here."))?
-        .execute(ResetColor)?;
-    
+    loop {
+        if read().unwrap() == Event::Key(esc) {
+            println!("Bye bye");
+            break
+        }
+    }
+    // let mut x = 0;
+    // let mut y = 0;
+    // execute!(stdout, SetSize(120, 40), Clear(ClearType::All))?;
+    // while y < 10 {
+    //     while x <= 20 {
+    //         execute!(
+    //             stdout, 
+    //             MoveTo(x,y),
+    //             Print("0"),
+    //         )?;
+    //         thread::sleep(Duration::from_millis(50));
+    //         x += 1;
+    //     }
+    //     execute!(stdout, Print("0"))?;
+    //     y += 1;
+    //     while x > 0 {
+    //         execute!(
+    //             stdout, 
+    //             MoveTo(x,y),
+    //             Print("0"),
+    //         )?;
+    //         thread::sleep(Duration::from_millis(50));
+    //         x -= 1;
+    //     }
+    //     execute!(stdout, MoveLeft(2), Print("0"))?;
+    //     y += 1
+    // }
     Ok(())
 }
